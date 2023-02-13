5c202ebbdb54d96e58d5e573fd9a23b837e837c4
i8james <124311130+i8james@users.noreply.github.com>
2023-02-13 13:40:29 -0800

    Unit Selection
    Starting On Unit Selection??? Probably don't need this if Justinian has already done this

---
+++ Iron Crest James/Assets/Scripts/UnitSelection.cs
@@ -0,0 +1,32 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitsSelection : MonoBehaviour
+{
+    private bool _isDraggingMouseBox = false;
+    private Vector3 _dragStartPosition;
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            _isDraggingMouseBox = true;
+            _dragStartPosition = Input.mousePosition;
+        }
+
+        if (Input.GetMouseButtonUp(0))
+            _isDraggingMouseBox = false;
+    }
+
+    void OnGUI()
+    {
+        if (_isDraggingMouseBox)
+        {
+            // Create a rect from both mouse positions
+            var rect = Utils.GetScreenRect(_dragStartPosition, Input.mousePosition);
+            Utils.DrawScreenRect(rect, new Color(0.5f, 1f, 0.4f, 0.2f));
+            Utils.DrawScreenRectBorder(rect, 1, new Color(0.5f, 1f, 0.4f));
+        }
+    }
+
+}
