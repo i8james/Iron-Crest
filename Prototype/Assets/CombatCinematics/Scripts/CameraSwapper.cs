529dc77ba7d1ce3a253ec43c3527396f7a5276cb
i8james <124311130+i8james@users.noreply.github.com>
2023-03-30 13:59:42 -0700

    Working on cinamachine

--- Prototype/Assets/CombatCinematics/Scripts/CameraSwapper.cs
+++ Prototype/Assets/CombatCinematics/Scripts/CameraSwapper.cs
@@ -5,15 +5,54 @@ using Cinemachine;
 
 public class CameraSwapper : MonoBehaviour
 {
-    // Start is called before the first frame update
+public List<Camera> Cameras;
+
     void Start()
     {
-        
+        EnableCamera(0);
+
+        var input = gameObject.GetComponent<InputField>();
+        var se = new InputField.SubmitEvent();
+        se.AddListener(SubmitName);
+        input.onEndEdit = se;
+
+        //or simply use the line below, 
+        //input.onEndEdit.AddListener(SubmitName);  // This also works
     }
 
-    // Update is called once per frame
-    void Update()
+    private void SubmitName(string arg0)
     {
-        
+        Debug.Log(arg0);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            EnableCamera(0);
+        }
+        else if (Input.GetKeyDown(KeyCode.X))
+        {
+            EnableCamera(1);
+        }
+        else if (Input.GetKeyDown(KeyCode.C))
+        {
+            EnableCamera(2);
+        }
+        else if (Input.GetKeyDown(KeyCode.V))
+        {
+            EnableCamera(3);
+        }
+
+        /*
+         * If you want to add more cameras, you need to add
+         * some more 'else if' conditions just like above
+         */
+    }
+
+    private void EnableCamera(int n)
+    {
+        Cameras.ForEach(cam => cam.enabled = false);
+        Cameras[n].enabled = true;
     }
 }
