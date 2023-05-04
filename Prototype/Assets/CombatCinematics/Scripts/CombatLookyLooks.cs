c4358efac52a69478fe3ef2cf67056025f6f145c
i8james <124311130+i8james@users.noreply.github.com>
2023-05-04 14:00:51 -0700

    Nearly done with combat cinematics
    Just a few hurdles left

--- Prototype/Assets/CombatCinematics/Scripts/CombatLookyLooks.cs
+++ Prototype/Assets/CombatCinematics/Scripts/CombatLookyLooks.cs
@@ -7,92 +7,59 @@ public class CombatLookyLooks : MonoBehaviour
 {
     //Camera Methods added by James
     //I dont want to break the script so ill add it as extra functions
-    private GameObject attackingUnit;
-    private GameObject defendingUnit;
-    private GameObject attackingVCam;
-    private GameObject defendingVCam;
-    public GameObject CombatCam;
+    public GameObject attackingUnit;
+    public GameObject defendingUnit;
+    public GameObject PerspectiveCam;
+    public GameObject CombatCams;
+    private GameObject Units;
+
     void Start()
     {
-        GetTheCameras();
-        CombatCam.gameObject.SetActive(true);
-        attackingVCam.gameObject.SetActive(false);
-        defendingVCam.gameObject.SetActive(false);
-    }
-
-    //Gets the cameras of the attacking and defending unit
-    public void GetTheCameras()
-    {
-        GetAttackerCam();
-        GetDefenderCam();
+        GetAttacker();
+        GetDefender();
+        PerspectiveCam.gameObject.SetActive(true);
     }
 
     //Finds the attackers vcam
-    public void GetAttackerCam()
+    public void GetAttacker()
     {
-        attackingUnit = GameObject.Find("/Units/Capsule");
-        attackingVCam = GameObject.Find("attackingUnit/attackingVCam");
+        attackingUnit = GameObject.Find("Capsule");
         //Transform childOfAnotherObject = attackingUnit.Find("attackingVCam");
         if (attackingUnit != null)
         {
-            Debug.Log("Attacking Unit Not found");
+            Debug.Log("Attacking Unit found");
         }
         else
         {
-            if (attackingVCam != null)
-            {
-                Debug.Log("Attacking Camera not found an error has occured");
-            }
-            else
-            {
-                Debug.Log("Attacking Camera Found");
-            }
+            Debug.Log("Attacking Unit Not Located");
         }
     }
 
     //Find the defenders vcam
-    public void GetDefenderCam()
+    public void GetDefender()
     {
         defendingUnit = GameObject.Find("/Units/Cylinder");
-        defendingVCam = GameObject.Find("defendingUnit/defendingVCam");
         //Transform childOfAnotherObject = defendingUnit.Find("defendingVCam");
         if(defendingUnit != null)
         {
-            Debug.Log("Defending Unit Not Found");
+            Debug.Log("Defending Unit Found");
         }
         else
         {
-            if (defendingVCam != null)
-            {
-                Debug.Log("Defending Camera not found an error has occured");
-            }
-            else
-            {
-                Debug.Log("Defending Camera Found");
-            }
+            Debug.Log("Defending Unit Not Located");
         }
     }
 
-    //Swaps the active camera to the attackers camera
-    public void SwapToAttackingCamera()
+    public void SwitchToMovingCam()
     {
-        attackingVCam.gameObject.SetActive(true);
-        //Attacking animations + Sounds trigger
-        Debug.Log("Pew Pew!!");
-    }
-
-    //Swaps the active camera to defending camera
-    public void SwapToDefendingCamera()
-    {
-        defendingVCam.gameObject.SetActive(true);
-        Debug.Log("Ouch i got hit!!!");
+        PerspectiveCam.gameObject.SetActive(true);
     }
     
     public void InitiateCombatCinematic()
     {
-        SwapToAttackingCamera();
-        SwapToDefendingCamera();
-        CombatCam.gameObject.SetActive(true);
+        PerspectiveCam.gameObject.SetActive(false);
+        //Combat Cams Initiates the combat blend
+        CombatCams.gameObject.SetActive(true);
         Debug.Log("Combat Finished...");
     }
 }
